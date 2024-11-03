f6deafd7b95e77722dcf4bce882066c570ad10a3
tramasys <29458406+tramasys@users.noreply.github.com>
2024-11-03 14:37:24 +0100

    Add script to generate trivial samples

---
+++ gen-trivial-samples.py
@@ -0,0 +1,16 @@
+import os
+import random
+import string
+
+os.makedirs("samples", exist_ok=True)
+
+samples = {
+    "a.txt": "a",
+    "aaa.txt": "a" * 100000,
+    "alphabet.txt": (string.ascii_lowercase * (100000 // 26 + 1))[:100000],
+    "random.txt": ''.join(random.choices(string.ascii_letters + string.digits, k=100000))
+}
+
+for filename, content in samples.items():
+    with open(os.path.join("samples", filename), "w") as file:
+        file.write(content)
