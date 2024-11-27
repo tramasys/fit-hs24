38f98d5bb2ee9877b70a3664d90a93d1e334e5dd
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-11-27 12:16:24 +0100

    improve generating of entropy samples

--- gen-entropy-samples.py
+++ gen-entropy-samples.py
@@ -1,16 +1,15 @@
 import os
 import random
-import string
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
 
-NUM_FILES = 1000
+NUM_FILES = 100
 FILE_SIZE = 100000
 
-def generate_text(entropy, length):
-    charset = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    charset = charset[:int(2 ** entropy)]
-    return ''.join(random.choice(charset) for _ in range(length))
+
+def generate_bytes(entropy, length):
+    byte_range = bytes(range(256))
+    max_byte_range = int(2 ** entropy)
+    selected_bytes = byte_range[:max_byte_range]
+    return bytes(random.choice(selected_bytes) for _ in range(length))
 
 
 def create_files(directory, num_files, file_size):
@@ -21,41 +20,12 @@ def create_files(directory, num_files, file_size):
 
     for i, entropy in enumerate(entropies):
         filename = f"file_{i:03d}_entropy.txt"
-        content = generate_text(entropy, file_size)
+        content = generate_bytes(entropy, file_size)
 
-        with open(os.path.join(directory, filename), "w") as file:
+        with open(os.path.join(directory, filename), "wb") as file:
             file.write(content)
 
         print(f"Created {filename}")
 
 
-# Create 100 files, each with 100,000 characters
 create_files("samples/entropy", NUM_FILES, FILE_SIZE)
-
-
-
-
-def encrypt_file(input_file, output_file, key):
-    with open(input_file, "rb") as f:
-        data = f.read()
-
-    initialization_vector = get_random_bytes(16)
-    cipher = AES.new(key, AES.MODE_CBC, initialization_vector)
-
-    padding_length = 16 - len(data) % 16
-    padded_data = data + bytes([padding_length]) * padding_length
-
-    ciphertext = cipher.encrypt(padded_data)
-
-    with open(output_file, "wb") as f:
-        f.write(initialization_vector + ciphertext)
-
-
-# encrypt every 10th file
-for i in range(0, NUM_FILES, 10):
-    input_filename = f"samples/entropy/file_{i:03d}_entropy.txt"
-    encrypted_filename = f"samples/entropy/encrypted_file_{i:03d}_entropy.txt"
-    key = get_random_bytes(32)
-    encrypt_file(input_filename, encrypted_filename, key)
-
-    print(f"Encrypted {input_filename} to {encrypted_filename}")
